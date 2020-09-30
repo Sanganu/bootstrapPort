3fb68a88858ceb5abdcfe9a1e322c8a91e7dcfa3
Sanganu <kppkmsanu@gmail.com>
2020-09-30 12:54:59 -0600

    Initial Files Setup change

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# bootstrapPort
+# BASIC PWA App Setup
+
+
+## Acknowledgements
+     ]
+
+
+### Developed By:
+Sangeetha.K
+Senior Tutor & Grader,
+Trilogy Education
--- public/script.js
+++ public/script.js
@@ -5,4 +5,26 @@
 // IndexedDB  - it is designed to work with callbacks and events
 
 
+const controlHTML = document.getElementById("controlSection")
+const displayHTML = controlHTML.firstElementChild
+const startHTML = controlHTML.children[1]
+const stopHTML = controlHTML.children[2]
+const resetHTML = controlHTML.children[3]
 
+startHTML.addEventListener("click",startTimer)
+stopHTML.onclick = stopTimer
+resetHTML.onclick = resetTimer
+
+
+function startTimer(){
+   console.log("START")
+}
+
+function stopTimer(){
+    console.log("STOP")
+
+}
+
+function resetTimer(){
+    console.log("RESET")
+}
