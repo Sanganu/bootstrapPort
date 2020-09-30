0ae8d490d9acca5205321d82e410951d0900b41e
Sanganu <kppkmsanu@gmail.com>
2020-09-30 10:11:55 -0600

    Initial File

---
+++ public/script.js
@@ -0,0 +1,8 @@
+//Indexed DB is non-relational database (NoSqL Database)
+//Stores: JS objects, files, blobs etc
+//Indexed DB provides Object Store
+// One Key per Object store - it helps to search data
+// IndexedDB  - it is designed to work with callbacks and events
+
+
+
